864eba6ab5e9a90cb1ce60c570921d7e69774ca5
Goran Djuric <gdjuric1989@gmail.com>
2018-10-02 17:40:35 +0200

    first commit

---
+++ weatherApp.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+	// get location
+
+	navigator.geolocation.getCurrentPosition(success, error);
+
+	function success(pos) {
+		var lat = pos.coords.latitude;
+		var long = pos.coords.longitude;
+
+		console.log(lat);
+		console.log(long);
+
+		weather(lat, long);
+	};
+
+	function error() {
+		console.log('greska');
+	}; 
+
+	function weather(lat, long) {
+		var url = `https://fcc-weather-api.glitch.me/api/current?lat=${lat}&lon=${long}`;
+		$.getJSON(url, function(data) {
+			console.log(data);
+			updateDom(data);
+		});
+	}
+
+	function updateDom(data) {
+		var city = data.name;
+		var temp = Math.round(data.main.temp);
+		var desc = data.weather[0].description;
+		var icon = data.weather[0].icon;
+		$('h2').html(city);
+		$('#num').html(temp);
+		$('#desc').html(desc)
+		$('img').attr('src', icon)
+	}
+});
